10c4e632e89083fa805cfe8caea8825654b8ec7d
Susanna Zandonai <susanna.zandonai@gmail.com>
2018-01-22 19:47:54 +0000

    first commit

---
+++ src/minesweeper.js
@@ -0,0 +1,10 @@
+const blankLine = '  |   |  ';
+const guessLine = '1 |   |  ';
+const bombLine = '  | B |  ';
+
+console.log('This is what an empty board would look like:');
+console.log(`${blankLine}\n${blankLine}\n${blankLine}`);
+console.log('This is what a board with a guess and a bomb on it would look like:');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
